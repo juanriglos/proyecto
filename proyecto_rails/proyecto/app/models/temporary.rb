9101f008e5be452601058711bdbd775b78a2003e
Juan Riglos <juanriglos11@gmail.com>
2016-12-08 02:36:02 -0300

    ahora temporary valida bien

--- proyecto_rails/proyecto/app/models/temporary.rb
+++ proyecto_rails/proyecto/app/models/temporary.rb
@@ -1,11 +1,14 @@
 class Temporary < Tarea
- validate :fecha_es_posible?
+ validates :fecha_es_posible? ,  presence: true
 
   def fecha_es_posible?
     if end_time < begin_time
+      Rails.logger.debug "FALSO"
        return false
     else
+      Rails.logger.debug "VERDADERO"
        return true
+
     end
   end
 end
