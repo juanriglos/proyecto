a1c78b9a2e414ee385531f8e8f6ee925dd393418
Juan Riglos <juanriglos11@gmail.com>
2016-12-10 00:58:02 -0300

    intentando hacer los test / comentado test/... x q sino no funcaba

--- proyecto_rails/proyecto/test/models/list_test.rb
+++ proyecto_rails/proyecto/test/models/list_test.rb
@@ -2,8 +2,35 @@ require 'test_helper'
 
 class ListTest < ActiveSupport::TestCase
 
-  def test_the_truth
-    assert true
+  test "no debe guardar una Lista sin nombre" do
+  		lista = List.new
+  		assert_not lista.save,"Se guardo una Lista sin nombre"
+	end
+
+  test "No debe guardar si la URL ya existe" do
+    lista_uno = List.new
+    lista_uno.URL = 'Test title'
+    lista_uno.save
+    lista_dos = List.new
+    lista_dos.URL = 'Test title'
+    assert lista_dos.invalid?
+  end
+
+  test "Devuelve las tareas ordenadas por prioridad" do
+    task_list = List.new
+    task_list.URL = 'Test url'
+
+    simple_task = Tarea.create(type:'Simple', description: 'Clean garage', priority: 2)
+    temporary_task = Tarea.create(type:'Temporary',description: 'Feed the wombat', priority: 3, begin_time: Date.today, end_time: Date.tomorrow)
+    long_task = Tarea.create(type:'Long', description: 'Shop for groceries', priority: 1)
+
+    [simple_task, temporary_task, long_task].each do |task|
+      task_list.tareas << task
+      task_list.save
+    end
+    lista_prioridad = task_list.tareas.order(priority: :desc)
+
+    assert_equal([long_task, simple_task, temporary_task], lista_prioridad, 'Wrong order!')
   end
 
 end
