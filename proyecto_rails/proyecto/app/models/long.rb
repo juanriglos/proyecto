cd86d5e97d44a3af8e516ff1a6182b0f567295c7
Juan Riglos <juanriglos11@gmail.com>
2016-12-10 16:28:49 -0300

    TP terminado

--- proyecto_rails/proyecto/app/models/long.rb
+++ proyecto_rails/proyecto/app/models/long.rb
@@ -1,7 +1,19 @@
 class Long < Tarea
+# validates :percentage, :numericality => { :less_than_or_equal_to => 100, :greater_than_or_equal_to => 0,
+# :only_integer => true}, :presence => true
 
-  validates_numericality_of :percentage,
-    :less_than_or_equal_to => 100,
-    :greater_than_or_equal_to => 0,
-    :only_integer => true
+validates :porcentaje? ,  presence: true
+
+ def porcentaje?
+   if  percentage <= 100 && percentage >= 0
+      return true
+   else
+      return false
+   end
+ end
+
+  # validates_numericality_of :percentage,
+  #   :less_than_or_equal_to => 100,
+  #   :greater_than_or_equal_to => 0,
+  #   :only_integer => true
 end
--- proyecto_rails/proyecto/test/models/list_test.rb
+++ proyecto_rails/proyecto/test/models/list_test.rb
@@ -39,7 +39,7 @@ class ListTest < ActiveSupport::TestCase
   test "Creacion de una tarea temporal con un rango de validez VALIDO" do
       lista = List.new
       lista.URL = 'prueba'
-      tarea = Tarea.create(type:'Temporary',description: 'Feed the wombat', priority: 3, begin_time: Date.today, end_time: Date.tomorrow)
+      tarea = Tarea.create(type:'Temporary',description: 'algo en la descripcion', priority: 3, begin_time: Date.today, end_time: Date.tomorrow)
       lista.tareas << tarea
       assert tarea.save , "SE GUARDO UNA TAREA CON RANGOS validos"
   end
@@ -55,7 +55,7 @@ class ListTest < ActiveSupport::TestCase
   test "El pasaje de una tarea temporal a estado Expirada" do
     lista = List.new
     lista.URL = 'prueba'
-    tarea = Tarea.create(type:'Temporary',description: 'Feed the wombat', priority: 3, begin_time: Date.today, end_time: Date.today, state:'Pendiente')
+    tarea = Tarea.create(type:'Temporary',description: 'algo en la descripcion', priority: 3, begin_time: Date.today, end_time: Date.today, state:'Pendiente')
     tarea.update(begin_time: Date.yesterday, end_time: Date.yesterday)
     lista.tareas << tarea
 
@@ -69,9 +69,9 @@ class ListTest < ActiveSupport::TestCase
     lista = List.new
     lista.URL = 'Test url'
 
-    tarea_simple = Tarea.create(type:'Simple', description: 'Clean garage', priority: 2)
-    tarea_temporaria = Tarea.create(type:'Temporary',description: 'Feed the wombat', priority: 3, begin_time: Date.today, end_time: Date.tomorrow)
-    tarea_larga = Tarea.create(type:'Long',percentage: 50, description: 'Shop for groceries', priority: 1)
+    tarea_simple = Tarea.create(type:'Simple', description: 'algo en la descripcion', priority: 2)
+    tarea_temporaria = Tarea.create(type:'Temporary',description: 'algo en la descripcion', priority: 3, begin_time: Date.today, end_time: Date.tomorrow)
+    tarea_larga = Tarea.create(type:'Long',percentage: 50, description: 'algo en la descripcion', priority: 1)
 
     [tarea_simple, tarea_larga, tarea_temporaria ].each do |task|
       lista.tareas << task
@@ -84,15 +84,35 @@ class ListTest < ActiveSupport::TestCase
   test "actualizacion de una tarea larga con porcentaje dentro del rango" do
     lista = List.new
     lista.URL = 'prueba'
-    tarea = Tarea.create(type:'Long',description: 'Feed the wombat', priority: 3,percentage: 50, state:'Pendiente')
+    tarea = Tarea.create(type:'Long',description: 'algo en la descripcion', priority: 3,percentage: 50, state:'Pendiente')
     tarea.update(percentage: 60)
     lista.tareas << tarea
 
     assert_equal(60, tarea.percentage, 'anduvo bien')
   end
 
-  test "actualizacion de una tarea larga con porcentaje fuera del rango" do
+  test "actualizacion de una tarea larga con porcentaje fuera del rango (-1)" do
+    lista = List.new
+    lista.URL = 'prueba'
+    tarea = Tarea.create(type:'Long',description: 'holalalala', priority: 3,percentage: 50, state:'Pendiente')
+    lista.tareas << tarea
 
+     tarea.update(percentage: 110)
+
+     tarea.percentage = -1
+     assert tarea.invalid?
+  end
+
+  test "actualizacion de una tarea larga con porcentaje fuera del rango (110)" do
+    lista = List.new
+    lista.URL = 'prueba'
+    tarea = Tarea.create(type:'Long',description: 'holalalala', priority: 3,percentage: 50, state:'Pendiente')
+    lista.tareas << tarea
+
+     tarea.update(percentage: 110)
+
+     tarea.percentage = 110
+     assert tarea.invalid?
   end
 
 
